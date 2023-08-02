2775701ea2b092683455506ebaaec5c70de06d65
AlfredoFAN <fan.alfredo@gmail.com>
2023-08-02 23:31:05 +1000

    contact form backend

--- netlify/functions/submit.js
+++ netlify/functions/submit.js
@@ -1,67 +1,44 @@
-const nodemailer = require('nodemailer');
+// netlify/submit.js
 
-const transporter = nodemailer.createTransport({
-  // Replace the SMTP transport options with your email service credentials
-  host: 'smtp-mail.outlook.com', // SMTP for Gmail: 'smtp.gmail.com'
-  port: 587,
-  secure: false,
-  auth: {
-    user: 'alfredofaustino@outlook.com', // Replace with your email address
-    pass: 'Qbasn8!23', // Replace with your email password or use environment variables for better security
-  },
-});
+const axios = require('axios');
 
-// Helper function to send email notification
-const sendEmailNotification = async (formData) => {
-  try {
-    const emailContent = `
-      New Contact Form Submission:
-
-      Name: ${formData.name}
-      Email: ${formData.email}
-      Message: ${formData.message}
-    `;
-
-    const mailOptions = {
-      from: 'alfredofaustino@outlook.com', // Replace with your email address
-      to: 'alfredofaustino@outlook.com', // Replace with your email address
-      subject: 'New Contact Form Submission',
-      text: emailContent,
-    };
-
-    await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.error('Failed to send email notification:', error);
-  }
-};
-
-// Lambda function to handle form submissions
-exports.handler = async (event, context) => {
+exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
-      body: JSON.stringify({ error: 'Method Not Allowed' }),
+      body: 'Method Not Allowed',
     };
   }
 
   try {
     const formData = JSON.parse(event.body);
 
-    // existing form validation logic
-    // ...
+    // Replace these variables with your email configuration
+    const emailRecipient = 'alfredofaustino@outlook.com'; // Replace this with your email
+    const emailSubject = 'New Contact Form Submission';
+    const emailBody = `
+      Name: ${formData.name}
+      Email: ${formData.email}
+      Message: ${formData.message}
+    `;
 
-    // Send email notification to the admin
-    await sendEmailNotification(formData);
+    // Send the email using a service of your choice
+    // For this example, we'll use Axios to send the email to a serverless email API
+    await axios.post('https://api.example.com/send-email', {
+      to: emailRecipient,
+      subject: emailSubject,
+      body: emailBody,
+    });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Form submission successful!' }),
+      body: JSON.stringify({ message: 'Form submission successful' }),
     };
   } catch (error) {
     console.error('Form submission failed:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal server error' }),
+      body: JSON.stringify({ message: 'Form submission failed' }),
     };
   }
 };
