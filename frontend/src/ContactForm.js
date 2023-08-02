68d26525ec339ba96169f007a96c8db8b1c088e0
AlfredoFAN <fan.alfredo@gmail.com>
2023-08-02 17:45:19 +1000

    backend contact form

--- frontend/src/ContactForm.js
+++ frontend/src/ContactForm.js
@@ -126,7 +126,7 @@ const ContactForm = () => {
       setIsSubmitting(true);
       try {
         const response = await axios.post(
-          '/.netlify/functions/submit', // Endpoint for Netlify function to handle form submissions
+          'http://localhost:8888/.netlify/functions/submit', // Endpoint for Netlify function to handle form submissions
           {
             ...formData,
             captchaToken,
