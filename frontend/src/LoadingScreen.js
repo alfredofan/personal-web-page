553377b7f6370b43aa8d0558fe8e5c52a85db812
AlfredoFAN <fan.alfredo@gmail.com>
2023-08-04 15:31:41 +1000

    curtain smoothness fix

--- frontend/src/LoadingScreen.js
+++ frontend/src/LoadingScreen.js
@@ -4,18 +4,16 @@ import styled, { keyframes } from 'styled-components';
 import logo from './img/personal_logo_white.png';
 
 const fadeInOut = keyframes`
-  0% {
+  0%, 100% {
     opacity: 0;
   }
   50% {
     opacity: 1;
   }
-  100% {
-    opacity: 0;
-  }
 `;
 
 const Logo = styled.a`
+  display: flex;
   align-self: center;
   justify-content: center;
   width: 106px;
@@ -30,48 +28,7 @@ const Logo = styled.a`
 //   background-color: white;
   animation: ${fadeInOut} 2s infinite;
   opacity: 0;
-
-  display: none; /* Hide the curtains on non-desktop devices */
-
-  @media (min-width: 768px) {
-    display: flex; /* Show the curtains on desktop devices */
-}
-
-@media (min-aspect-ratio: 21/10) { //used 21:10.5 instead of 21:9 because of different mobile browsers
-   display: none; /* Hide the curtains on non-mobile devices */
-}
 `;
-
-
-
-const LogoMobile = styled.a`
-  align-self: center;
-  justify-content: center;
-  margin-bottom: 25vh;
-  width: 106px;
-  height: 43px;
-  position: absolute;
-  background: transparent;
-  color: #fff;
-  transition: background 0.3s ease;
-  background-image: url(${logo});
-  background-repeat: no-repeat;
-  background-size: contain;
-//   background-color: white;
-  animation: ${fadeInOut} 2s infinite;
-  opacity: 0;
-
-  display: none; /* Hide the curtains on non-desktop devices */
-
-  @media (max-width: 768px) {
-    display: flex; /* Show the curtains on mobile devices */
-}
-
-@media (min-aspect-ratio: 21/10) {
-  display: none; /* Hide the curtains on non-mobile devices */
-}
-`;
-
 
 const LoadingScreenContainer = styled.div`
     display: flex;
@@ -89,8 +46,6 @@ const LoadingScreen = () => {
     <LoadingScreenContainer >
       {/* <div className="loader"></div> */}
       <Logo />
-      <LogoMobile />
-
     </LoadingScreenContainer>
   );
 };
